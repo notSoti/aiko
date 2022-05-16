55c2f3b9522880a649cc224d5d0360896142a465
Sotiris <58138432+SotiTheKing@users.noreply.github.com>
2022-05-16 19:49:36 +0000

    minor changes (ver, startup processes/web)

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "<code>This is webserver for modmail bot hosted on replit.<br>Guide: <a href='https://go.anondev.ml/modmail-replit' target='__blank'>go.anondev.ml/modmail-replit</a></code>"
+    return "Aiko! prod version. If you're somehow seeing this then hi you could say this is an easter egg!"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
