07aea8cf4cd3189a8ed3ae4b6111f31a4bd94919
Soti <sotosz04@yahoo.gr>
2022-10-27 21:02:24 +0300

    new restart command

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -2604,21 +2604,38 @@ class Modmail(commands.Cog):
       await bot.add_cog(automod_cmds(bot))
 
 
-    class restart(commands.Cog):
-      def __init__(self, bot):
+class RebootCog(commands.Cog):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    @checks.has_permissions(PermissionLevel.ADMIN)
-    @commands.cooldown(1, 180, BucketType.user)
-    async def restart(self, ctx):
+    @checks.has_permissions(PermissionLevel.OWNER)
+    async def reboot(self, ctx):
+        """Clears Cached Logs & Reboots The Bot"""
+        msg = await ctx.send(embed=discord.Embed(
+            color=self.bot.main_color,
+            description="Processing..."
+        ))
 
-      await ctx.channel.send("Restarting.")
-      system("kill 11") 
+        # Clear The cached logs
+        #with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+       #                        '../../../temp/logs.log'), 'w'):
+          #  pass
+        await ctx.invoke(self.bot.get_command('debug clear'))
+        emsg = await msg.edit(embed=discord.Embed(
+            color=self.bot.main_color,
+            description="✅ Cleared Cached Logs"
+        ))
+        logger.info("==== Rebooting Bot ====")
+        await msg.edit(embed=discord.Embed(
+            color=self.bot.main_color,
+            description="`✅ | Cleared Cached Logs`\n\n`✅ | Rebooting....`"
+        ))
+        os.execl(sys.executable, sys.executable, * sys.argv)
 
 
-    async def setup(bot):
-      await bot.add_cog(restart(bot))
+def setup(bot):
+    bot.add_cog(RebootCog(bot))
 
 
     class partner(commands.Cog):
@@ -4060,5 +4077,3 @@ class Modmail(commands.Cog):
 async def setup(bot):
     await bot.add_cog(Modmail(bot))
 
-
-# test
